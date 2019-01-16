d737977dc849752fc17e396bccde18b4cb4fb6ee
Jason.But <jason.but@yahoo.com>
2019-01-16 20:09:24 +0800

    调整项目结构

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,20 @@ export default {
   name: 'app'
 };
 </script>
+
+<style lang="scss">
+html,body {
+  box-sizing: border-box;
+  margin: 0 0 0 0;
+  padding: 0;
+  font-family: Tahoma, Arial, Helvetica, sans-serif,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;
+
+  .icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+    overflow: hidden;
+  }
+}
+</style>
