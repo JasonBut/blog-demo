5fbeb2d6219eb40b374b0ee402bec23dc4bea0f1
Jason.But <jason.but@yahoo.com>
2019-01-21 02:01:09 +0800

    修正面包屑导航抖动

--- src/Components/Front/FrontPost/FrontPostDetails/index.vue
+++ src/Components/Front/FrontPost/FrontPostDetails/index.vue
@@ -7,28 +7,11 @@
 </template>
 
 <script>
-import { mapMutations } from 'vuex';
-import { Types } from '@/Store';
 export default {
   name: 'FrontPostDetails',
   components: {
     DataDisplayHOC: () => import('@/Components/Commons/DataDisplayHOC'),
     PostDetailsUI: () => import('./FrontPostDetailsUI')
-  },
-
-  // 离开当前详情页时清理state中post内容,避免面包屑导航中文章标题项抖动
-  beforeRouteLeave (to, from, next) {
-    this.updateStore({
-      target: 'post',
-      data: {}
-    });
-    next();
-  },
-
-  methods: {
-    ...mapMutations({
-      updateStore: Types.UPDATE_STORE
-    })
   }
 };
 </script>
--- src/Components/Front/FrontPost/index.vue
+++ src/Components/Front/FrontPost/index.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { mapState, mapMutations } from 'vuex';
+import { Types } from '@/Store';
 export default {
   name: 'FrontPost',
 
@@ -19,6 +20,21 @@ export default {
   },
   computed: {
     ...mapState(['post'])
+  },
+
+  // 离开当前详情页时清理state中post内容,避免面包屑导航中文章标题项抖动
+  beforeRouteLeave (to, from, next) {
+    this.updateStore({
+      target: 'post',
+      data: {}
+    });
+    next();
+  },
+
+  methods: {
+    ...mapMutations({
+      updateStore: Types.UPDATE_STORE
+    })
   }
 };
 </script>
