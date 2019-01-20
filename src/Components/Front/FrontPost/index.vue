fcb055a49eafe6eef8bf214df036db7fd44305de
Jason.But <jason.but@yahoo.com>
2019-01-20 13:17:01 +0800

    把离开详情页清除post状态的方法转移到PostDetails里

--- src/Components/Front/FrontPost/index.vue
+++ src/Components/Front/FrontPost/index.vue
@@ -8,8 +8,7 @@
 </template>
 
 <script>
-import { mapState, mapMutations } from 'vuex';
-import { Types } from '@/Store';
+import { mapState } from 'vuex';
 export default {
   name: 'FrontPost',
 
@@ -18,24 +17,8 @@ export default {
     Comments: () => import('./FrontPostComments'),
     Editor: () => import('@/Components/Commons/Editor')
   },
-
   computed: {
     ...mapState(['post'])
-  },
-
-  // 离开当前详情页时清理state中post内容,避免面包屑导航中文章标题项抖动
-  beforeRouteLeave (to, from, next) {
-    this.updateStore({
-      target: 'post',
-      data: {}
-    });
-    next();
-  },
-
-  methods: {
-    ...mapMutations({
-      updateStore: Types.UPDATE_STORE
-    })
   }
 };
 </script>
