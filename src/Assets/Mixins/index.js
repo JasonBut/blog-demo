a2a100e9963b73c7ceabc6e5c342863a760489f0
Jason.But <jason.but@yahoo.com>
2019-01-29 13:54:20 +0800

    调整结构

--- src/Assets/Mixins/index.js
+++
@@ -1,9 +0,0 @@
-// 公用
-export PaginationMixin from '../../Util/mixins/paginationMixin';
-export CleanPostMixins from '../../Util/mixins/cleanPostMixins';
-export RequestFailed from '../../Util/mixins/requestFailed';
-
-// 后台
-export ArticleEdit from '../../Util/mixins/articleEdit';
-export ScrollToItem from '../../Util/mixins/scrollToItem';
-export DeleteDataMixin from '../../Util/mixins/deleteDataMixin';
